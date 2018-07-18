baf22f7f2d3e1c5d17860e95e89303ca933bcb56
kunalJa <kunaljain@utexas.edu>
2018-07-18 18:15:35 -0500

    Chaining Promises and using .catch

--- playground/promise.js
+++ playground/promise.js
@@ -1,12 +1,33 @@
-const somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('Hey it worked');
-    reject('Didn\'t work');
-  }, 2500);
-});
+const asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('You must input numbers.');
+      }
+    }, 1500);
+  });
+};
 
-somePromise.then((message) => {
-  console.log('Success: ', message);
-}, (error) => {
-  console.log('Error: ', error);
+asyncAdd(21,9).then((res) => {
+  console.log(res);
+  return asyncAdd(res, 10);
+}).then((res) => {
+  console.log(res);
+}).catch((e) => {
+  console.log(e);
 })
+
+// const somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('Hey it worked'); // Only one action can happen
+//     reject('Didn\'t work');
+//   }, 2500);
+// });
+
+// somePromise.then((message) => {
+//   console.log('Success: ', message);
+// }, (error) => {
+//   console.log('Error: ', error);
+// })
